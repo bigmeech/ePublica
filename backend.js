f263dd8af1e02602cb4a35808bb0cf2c4c23eb10
bigmeech <denachural@gmail.com>
2014-05-12 19:07:59 +0100

    Added Cropper component, New express routes, and added OAuth Module

--- backend.js
+++ backend.js
@@ -4,11 +4,12 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var cons = require("consolidate");
+
+//routes definition
+var users = require('./lib/users/routes')
 
 var app = express();
 
@@ -34,8 +35,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', users.index);
+app.get('/users', users.getUser);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
