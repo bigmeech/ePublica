a9d6b93ad9b3b4e5cd5920f406fdd06f80f48a00
bigmeech <denachural@gmail.com>
2014-05-16 08:10:56 +0100

    Added Home Component, New Markup for index.html, added passport modules for OAuth, created book, author and user mongoose schema

--- backend.js
+++ backend.js
@@ -7,13 +7,26 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var cons = require("consolidate");
+var authServer = require("oauthorize").createServer();
+var passport = require("passport");
+var localStrategy = require("passport-local").Strategy;
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/epublica");
+//db models
+var UserModel = null;
+var AuthorModel = null;
+var BookModel = null;
+
 
 //routes definition
-var users = require('./lib/users/routes')
-
+var users = require('./lib/users/routes');
+var pass = require('./lib/auth/routes');
+//console.log(typeof pass);
 var app = express();
+var staticDir = path.join(__dirname, "/public");
 
-var staticDir = path.join(__dirname, "/public")
+
 
 // all environments
 app.configure(function(){
@@ -30,6 +43,12 @@ app.configure(function(){
     app.use(express.session());
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new localStrategy({
+      usernameField:"email",
+      passwordField:"password"
+    },pass.verifier));
 })
 
 // development only
@@ -37,8 +56,48 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//mongodb connections
+//--------------------------------------------------
+var db = mongoose.connection
+db.on("open",function(){
+  //On Open suceed,
+  var userSchema = mongoose.Schema({
+    username:String,
+    password:String,
+    email:String
+  });
+
+  var authorSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    dateCreated:Date
+  });
+
+  var bookSchema = mongoose.Schema({
+    title:String,
+    synopsis:String,
+    price: Number,
+    rating:Number
+  });
+  UserModel = mongoose.model('user',userSchema);
+  AuthorModel = mongoose.model('author',authorSchema);
+  BookModel = mongoose.model('book',bookSchema);
+})
+
+//on DB open error
+//----------------------------------------------
+db.on("error",function(err){
+  console.log("Error Opening mongodb"+err)
+});
+
+//express route definitions
+//----------------------------------------------
 app.get('/', function(req,res){res.render('index')});
 app.get('/users', users.getUser);
+app.post('/login',
+    passport.authenticate('local',{
+  successRedirect:'/main',
+  failureRedirect:'/login'}));
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
