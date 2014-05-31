0c3ffcc0240b7781fc464481432aab39400a593c
bigmeech <denachural@gmail.com>
2014-05-31 23:44:44 +0100

    Changed launcher script, Added blur filter for all modal dialog overlays

--- backend.js
+++ backend.js
@@ -34,7 +34,7 @@ app.configure(function(){
     app.use(express.urlencoded());
     app.use(express.methodOverride());
     app.use(express.cookieParser('your secret here'));
-    app.use(express.session());
+    app.use(express.session({secret:"Opensesamie85"}));
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(passport.initialize());
