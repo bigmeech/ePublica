8f5582e0408fbec68d1a6aa1af16d07def653dd7
epublica <denachural@gmail.com>
2014-07-16 02:05:29 +0100

    updated gulp file, added watch for stylus and angularjs modules, added liver reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,39 @@
 var gulp                    = require("gulp"),
     gulpStylus              = require("gulp-stylus"),
     gulpComponent           = require("gulp-component"),
-    gulpComponentStylus     = require("component-stylus-plugin");
-    gulpKarma               = require("gulp-karma")
+    componentStylus         = require("component-stylus");
+    gulpKarma               = require("gulp-karma");
+    liveReload              = require("gulp-livereload");
     karmaFiles              = [ "/lib/angular-1.2.9.js",
                                 "/lib/angular-mock-1.2.9.js",
                                 "/public/build/build.js",
                                 "/lib/editor/test/*"]
 
 
+//builds application styles from various stylus templates into a final build.css
 gulp.task("build:component-styles", function(){
     gulp.src('component.json')
         .pipe(gulpComponent.styles({
             configure:function(builder){
-                builder.use(gulpComponentStylus);
-            }
+                builder.use(componentStylus);
+            },
+            out:'public/build'
         }))
-        .pipe(gulp.dest('public/build'));
+        .pipe(gulp.dest('public/build'))
+        .pipe(liveReload())
 });
 
+//builds application components together from various angularjs components into a final build.js file
 gulp.task("build:component-scripts", function(){
     gulp.src('component.json')
         .pipe(gulpComponent.scripts({
             standalone:true
         }))
-        .pipe(gulp.dest('public/build'));
+        .pipe(gulp.dest('public/build'))
+        .pipe(liveReload())
 });
 
+//runs karma using phantomjs as browser . See karma.conf file
 gulp.task("run:karma", function(){
     return gulp.src(karmaFiles)
         .pipe(gulpKarma({
@@ -34,12 +41,21 @@ gulp.task("run:karma", function(){
             action:'run'
         }))
         .on('error',function(err){
-            throw error;
+            throw err;
         })
 });
 
-gulp.task('watch',function(){
+//sets up styls, scripts and testpects for watch
+gulp.task('watch', function(){
+    gulp.watch(['component.json','lib/**/*.js'],['build:component-scripts']);
+    gulp.watch(['component.json','lib/**/*.styl'],['build:component-styles']);
+});
 
+//report error if any
+gulp.task('errors',function(){
+    gulp.src('.css/errors.styl')
+        .pipe(gulpStylus({errors:true}))
+        .pipe(gulp.dest('./css'));
 })
 
-gulp.task("default", ['build:component-styles','build:component-scripts','run:karma']);
+gulp.task("default", ['build:component-styles','build:component-scripts','run:karma','watch']);
